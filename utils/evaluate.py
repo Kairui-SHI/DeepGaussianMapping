8c8fd1750c0247c5613b288b06aaf6c174ec21ee
Kairui-SHI <2977493767@qq.com>
2024-06-17 22:20:55 +0800

    update evaluate

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -63,53 +63,61 @@ def color_point_cloud_by_mme(point_cloud, entropies, min_abs_entropy, max_abs_en
         colored_point_cloud.colors.append(color)
     return colored_point_cloud
 
-def MME():
+def MME(dir=None):
     print('load pcd ...')
-    # gt_pcd = o3d.io.read_point_cloud('./pcd_save/gt_pcd.ply')
-    Lnet_est_pcd = o3d.io.read_point_cloud('./pcd_save/Lnet_pcd_est.ply')
-    # init_pcd = o3d.io.read_point_cloud('./pcd_save/init_pcd.ply')
-    # o3d.visualization.draw_geometries([pointcloud])
+    if dir is not None:
+        gt_pcd = o3d.io.read_point_cloud(dir+'gt_pcd.ply')
+        Lnet_est_pcd = o3d.io.read_point_cloud(dir+'Lnet_pcd_est.ply')
+        init_pcd = o3d.io.read_point_cloud(dir+'init_pcd.ply')
+        # o3d.visualization.draw_geometries([pointcloud])
+    else:
+        assert False
 
     # print('compute mean map entropy ...')
     # radius = 0.1
     # est_mme, est_entropies = compute_mean_map_entropy(Lnet_est_pcd, radius)
-    # # gt_mme, gt_entropies = compute_mean_map_entropy(gt_pcd, radius)
-    # # init_mme, init_entropies = compute_mean_map_entropy(init_pcd, radius)
+    # gt_mme, gt_entropies = compute_mean_map_entropy(gt_pcd, radius)
+    # init_mme, init_entropies = compute_mean_map_entropy(init_pcd, radius)
 
-    # np.savez('pcd_save/mean_map_entropy_replica.npz', est_entropies=est_entropies, est_mme=est_mme)
+    # # np.savez(dir+'mean_map_entropy.npz', est_entropies=est_entropies, est_mme=est_mme)
 
-    # data = np.load('pcd_save/mean_map_entropy.npz')
-    # est_entropies = data['est_entropies']
-    # gt_entropies = data['gt_entropies']
-    # est_mme = data['est_mme']
-    # gt_mme = data['gt_mme']
-    # np.savez('pcd_save/mean_map_entropy.npz', est_entropies=est_entropies, gt_entropies=gt_entropies, init_entropies=init_entropies, est_mme=est_mme, gt_mme=gt_mme, init_mme=init_mme)
+    # # data = np.load('pcd_save/mean_map_entropy.npz')
+    # # est_entropies = data['est_entropies']
+    # # gt_entropies = data['gt_entropies']
+    # # est_mme = data['est_mme']
+    # # gt_mme = data['gt_mme']
+    # np.savez(dir+'mean_map_entropy.npz', est_entropies=est_entropies, gt_entropies=gt_entropies, init_entropies=init_entropies, est_mme=est_mme, gt_mme=gt_mme, init_mme=init_mme)
 
     # print(f"Estimated MME: {est_mme}")
     # print(f"GT MME: {gt_mme}")
     # print(f"Init MME: {init_mme}")
 
     print('color point cloud by mme ...')
-    data = np.load('pcd_save/mean_map_entropy.npz')
-    data2 = np.load('pcd_save/mean_map_entropy_replica.npz')
-    est_entropies = data2['est_entropies']
-    # gt_entropies = data['gt_entropies']
+    data = np.load(dir+'mean_map_entropy.npz')
+    est_entropies = data['est_entropies']
+    gt_entropies = data['gt_entropies']
     init_entropies = data['init_entropies']
-    est_mme = data2['est_mme']
-    # gt_mme = data['gt_mme']
+    mask = est_entropies != 0
+    print(f"Estimated entropies max: {np.max(est_entropies[mask])} min: {np.min(est_entropies[mask])}")
+    mask = gt_entropies != 0
+    print(f"GT entropies max: {np.max(gt_entropies[mask])} min: {np.min(gt_entropies[mask])}")
+    mask = init_entropies != 0
+    print(f"Init entropies max: {np.max(init_entropies[mask])} min: {np.min(init_entropies[mask])}")
+    est_mme = data['est_mme']
+    gt_mme = data['gt_mme']
     init_mme = data['init_mme']
-    min_abs_entropy = 7.5
-    max_abs_entropy = 13.0
+    min_abs_entropy = 7.0
+    max_abs_entropy = 11.0
     # set invalid entropies to max
-    est_entropies[est_entropies == 0] = max_abs_entropy
-    # gt_entropies[gt_entropies == 0] = max_abs_entropy
-    init_entropies[init_entropies == 0] = max_abs_entropy
+    est_entropies[est_entropies == 0] = -max_abs_entropy
+    gt_entropies[gt_entropies == 0] = -max_abs_entropy
+    init_entropies[init_entropies == 0] = -max_abs_entropy
 
     print(f"Estimated MME: {est_mme}")
-    # print(f"GT MME: {gt_mme}")
+    print(f"GT MME: {gt_mme}")
     print(f"Init MME: {init_mme}")
-    # entropies = np.concatenate((est_entropies, gt_entropies, init_entropies))
-    entropies = np.concatenate((est_entropies, init_entropies))
+    entropies = np.concatenate((est_entropies, gt_entropies, init_entropies))
+    # entropies = np.concatenate((est_entropies, init_entropies))
     # min_abs_entropy = np.min(np.abs(entropies))
     # max_abs_entropy = np.max(np.abs(entropies))
 
@@ -123,28 +131,28 @@ def MME():
     sm.set_array([])  # Set an empty array to avoid error
     fig.colorbar(sm, ax=ax)
     plt.show()
-    # # init
-    # colored_point_cloud = o3d.geometry.PointCloud()
-    # colored_point_cloud = color_point_cloud_by_mme(init_pcd, init_entropies, min_abs_entropy, max_abs_entropy)
-    # o3d.visualization.draw_geometries([colored_point_cloud])
-    # o3d.io.write_point_cloud("./pcd_save/mme_init_pcd.ply", colored_point_cloud)
+    # init
+    colored_point_cloud = o3d.geometry.PointCloud()
+    colored_point_cloud = color_point_cloud_by_mme(init_pcd, init_entropies, min_abs_entropy, max_abs_entropy)
+    o3d.visualization.draw_geometries([colored_point_cloud])
+    o3d.io.write_point_cloud(dir+'mme_init_pcd.ply', colored_point_cloud)
     # est
     colored_point_cloud = o3d.geometry.PointCloud()
     colored_point_cloud = color_point_cloud_by_mme(Lnet_est_pcd, est_entropies, min_abs_entropy, max_abs_entropy)
     o3d.visualization.draw_geometries([colored_point_cloud])
-    o3d.io.write_point_cloud("./pcd_save/mme_est_pcd.ply", colored_point_cloud)
-    # # gt
-    # colored_point_cloud = o3d.geometry.PointCloud()
-    # colored_point_cloud = color_point_cloud_by_mme(gt_pcd, gt_entropies, min_abs_entropy, max_abs_entropy)
-    # o3d.visualization.draw_geometries([colored_point_cloud])
-    # o3d.io.write_point_cloud("./pcd_save/mme_gt_pcd.ply", colored_point_cloud)
+    o3d.io.write_point_cloud(dir+'mme_est_pcd.ply', colored_point_cloud)
+    # gt
+    colored_point_cloud = o3d.geometry.PointCloud()
+    colored_point_cloud = color_point_cloud_by_mme(gt_pcd, gt_entropies, min_abs_entropy, max_abs_entropy)
+    o3d.visualization.draw_geometries([colored_point_cloud])
+    o3d.io.write_point_cloud(dir+'mme_gt_pcd.ply', colored_point_cloud)
 
 def APE():
     # calculate absolute pose error with gt
     # load data
-    Lnet_est_pose = np.load(f"./pcd_save/room0_0/GSsetup_Lnet_est_pose_eval.npy")
-    Initial_pose = np.load(f"./pcd_save/room0_0/Initial_pose.npy")
-    gt_pose = np.load(f"./pcd_save/room0_0/gt_all_frames_c2w.npy")
+    Lnet_est_pose = np.load(f"./pcd_save/apartment_0/GSsetup_Lnet_est_pose_eval.npy")
+    Initial_pose = np.load(f"./pcd_save/apartment_0/initial_pose.npy")
+    gt_pose = np.load(f"./pcd_save/apartment_0/gt_all_frames_c2w.npy")
     
     q = Lnet_est_pose[:,3:]
     q = q[:, [1, 2, 3, 0]]
@@ -189,4 +197,6 @@ def APE():
     print(f"Initial APE_T: {ape_T}, APE_R: {ape_R}, Total APE: {ape}" )
 
 if __name__ == "__main__":
-    APE()
+    dir = "./results/apartment_0/"
+    MME(dir)
+    # APE()
